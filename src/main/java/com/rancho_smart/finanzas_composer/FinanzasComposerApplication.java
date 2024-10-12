4df2421c06e1fef6aeadd496d0f55a037aeaae9d
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 04:47:34 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ src/main/java/com/rancho_smart/finanzas_composer/FinanzasComposerApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.finanzas_composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzasComposerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rancho_smart/finanzas_composer/FinanzasComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.finanzas_composer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasComposerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
