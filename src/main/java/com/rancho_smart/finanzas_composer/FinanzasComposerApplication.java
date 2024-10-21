d116276e39fe88c43aa61fc59950e18bb9f22de7
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-21 03:21:50 -0500

    :wrench: chore: Configuración con Docker y Docker Compose

--- src/main/java/com/rancho_smart/finanzas_composer/FinanzasComposerApplication.java
+++ src/main/java/com/rancho_smart/finanzas_composer/FinanzasComposerApplication.java
@@ -2,8 +2,10 @@ package com.rancho_smart.finanzas_composer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FinanzasComposerApplication {
 
 	public static void main(String[] args) {
